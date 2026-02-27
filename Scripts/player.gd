b91e6e2e34827f697bfcef1c7f3d14a3002a6588
Iobidze <111024323+Iobidze@users.noreply.github.com>
2026-02-27 23:19:08 +0300

    Gera
    123

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 @export var speed = 300
 
+func _ready():
+	$Area2D.body_entered.connect(_on_area_entered)
+	
+func _on_area_entered(body):
+	if body.is_in_group("Angel"):
+		await get_tree().process_frame
+		get_tree().reload_current_scene()
 
 func _physics_process(_delta):
 	# Движение (WASD)
