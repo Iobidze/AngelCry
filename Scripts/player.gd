2868e0fdabb5c5543257bcea4c44a4294f536c99
Iobidze <111024323+Iobidze@users.noreply.github.com>
2026-03-01 00:03:44 +0300

    Gera
    123

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,7 @@ func _ready():
 func _on_area_entered(body):
 	if body.is_in_group("Angel"):
 		await get_tree().process_frame
-		get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
 
 func _physics_process(_delta):
 	# Движение (WASD)
@@ -17,3 +17,8 @@ func _physics_process(_delta):
 
 	# Поворот за мышкой (фонарик будет светить куда смотришь)
 	look_at(get_global_mouse_position())
+
+
+func _on_touch_screen_button_pressed():
+	
+	pass # Replace with function body.
